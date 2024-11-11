01ad281f46460348571e4ab6ff4d6ab7d1dd8db7
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-11-11 12:21:21 +0100

    Revert deprecating EmptyPropertyListExtractor

--- PropertyInfo/Extractor/EmptyPropertyListExtractor.php
+++ PropertyInfo/Extractor/EmptyPropertyListExtractor.php
@@ -15,14 +15,6 @@ namespace Sylius\Bundle\ApiBundle\PropertyInfo\Extractor;
 
 use Symfony\Component\PropertyInfo\PropertyListExtractorInterface;
 
-trigger_deprecation(
-    'sylius/api-bundle',
-    '1.14',
-    'The "%s" class is deprecated and will be removed in Sylius 2.0.',
-    EmptyPropertyListExtractor::class,
-);
-
-/** @deprecated since Sylius 1.14 and will be removed in Sylius 2.0. */
 final class EmptyPropertyListExtractor implements PropertyListExtractorInterface
 {
     public function getProperties($class, array $context = []): ?array
