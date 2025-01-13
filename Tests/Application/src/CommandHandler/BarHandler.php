c765e56ca24f011e68c94c4617d849f3dd03527a
Francis Hilaire <Prometee@users.noreply.github.com>
2025-01-13 18:16:07 +0100

    Add Bar api resource to simulate input class change

---
+++ Tests/Application/src/CommandHandler/BarHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ApiBundle\Application\CommandHandler;
+
+use Bar;
+use Sylius\Bundle\ApiBundle\Application\Command\BarCommand;
+use Sylius\Component\Core\Model\ShopUserInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+final readonly class BarHandler
+{
+    public function __invoke(BarCommand $command): Bar
+    {
+       return new Bar();
+    }
+}
